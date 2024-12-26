46477f1d267ee5d200b39db0cfa7a554406d8f47
Mnemon <mail.myousefzadeh@gmail.com>
2024-12-26 23:04:27 +0330

    adds main.css and some changes to the js file

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([35, 50], 5);
 
 var osmLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OSM'
